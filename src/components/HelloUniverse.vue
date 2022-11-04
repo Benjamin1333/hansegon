8f2ca2813122b73a23753315f06d051b3a093ecc
benjaminruehl <ruehl@lotum.de>
2022-11-04 17:11:13 +0100

    Hugendubel

---
+++ src/components/HelloUniverse.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>{{msg}}</div>
+</template>
+
+<script lang="ts">
+import Component from "vue-class-component";
+import {Prop} from "vue-property-decorator";
+import Vue from "vue";
+
+@Component
+export default class HelloUniverse extends Vue {
+
+  @Prop({ default: 'default value' }) readonly msg!: string
+
+}
+</script>
+
+<style scoped>
+
+</style>
